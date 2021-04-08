a08240802de44f0e943e532254b4e9b6772c7da5
Alonso-Pablo <sidokunoice@gmail.com>
2021-04-08 13:35:09 -0300

    Advances JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 const menuIconDisplay = document.getElementById("menu-icon");
+const mainMenu = document.getElementById("main-menu");
+
+function replaceClass (objetiveElement, classAdd, classRemove) {
+    
+    if ( objetiveElement.classList.item(1) === classRemove) {
+        objetiveElement.classList.add(classAdd);
+        objetiveElement.classList.remove(classRemove);
+    } else {
+        objetiveElement.classList.remove(classAdd)
+        objetiveElement.classList.add(classRemove);
+    }
+}
+
+menuIconDisplay.addEventListener("click", () => {
+    replaceClass (mainMenu, "displayFlex", "displayNone");
+});
