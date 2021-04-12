9a6b43a75ae5eff918c980561045fec2b48270c1
Alonso-Pablo <sidokunoice@gmail.com>
2021-04-12 01:22:46 -0300

    Finishing index.html for cellphone and tablets

--- js/index.js
+++ js/index.js
@@ -1,16 +1,4 @@
 
-/* NAV */
-const searchMenu = document.getElementById("search");
-const mainMenu = document.getElementById("main-menu");
-
-    /* Buttons */
-const searchIcon = document.getElementById("search-icon");
-
-const menuIconDisplay = document.getElementById("menu-icon");
-const closeMenu = document.getElementById("close-menu-icon");
-
-
-
 function replaceClass (objetiveElement, classAdd, classRemove) {
     if ( objetiveElement.classList.item(1) === classRemove) {
         objetiveElement.classList.add(classAdd);
@@ -21,15 +9,41 @@ function replaceClass (objetiveElement, classAdd, classRemove) {
     }
 }
 
+/* NAV */
+const searchMenu = document.getElementById("search");
+const mainMenu = document.getElementById("main-menu");
+
+    /* Buttons */
+const searchIcon = document.getElementById("search-icon");
+
+const menuIconDisplay = document.getElementById("menu-icon");
+const closeMenu = document.getElementById("close-menu-icon");
+
+    /* Functions */
+    
+        /* Search Menu - Display or Hide */
 searchIcon.addEventListener("click", () => {
     replaceClass (searchMenu, "displayFlex", "displayHidden");
 });
 
+        /* Option menu - Display or Hide */
 menuIconDisplay.addEventListener("click", () => {
     replaceClass (mainMenu, "displayFlex", "displayNone");
 });
-
 closeMenu.addEventListener("click", () => {
     mainMenu.classList.remove("displayFlex");
     mainMenu.classList.add("displayNone");
 });
+
+
+/* MAIN */
+const mainDisclaimer = document.getElementById("main__disclaimer");
+
+    /* Buttons */
+
+    /* Functions */
+        /* main > Disclaimer - Display or Hide */
+mainDisclaimer.addEventListener("click", () => {
+    replaceClass (mainDisclaimer, "displayFlex", "displayNone");
+})
+
