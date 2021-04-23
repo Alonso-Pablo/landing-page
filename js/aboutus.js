300c58be18bc486bc500cf9d6eef3f7dfab18e5c
Alonso-Pablo <sidokunoice@gmail.com>
2021-04-23 18:33:07 -0300

    Changes in all html, nav

--- js/aboutus.js
+++ js/aboutus.js
@@ -9,20 +9,57 @@ function replaceClass (objetiveElement, classAdd, classRemove) {
     }
 };
 
+function openNewTab () {
+    if (inputCellphone.value != "" && inputCellphone.value != " ") { 
+        window.location.href = `products.html#${inputCellphone.value}`;      
+    } else if (inputTablet.value != "" && inputTablet.value != " ") {
+        window.location.href = `products.html#${inputTablet.value}`;
+    } else if (inputDesktop.value != "" && inputDesktop.value != " ") {
+        window.location.href = `products.html#${inputDesktop.value}`;
+    }
+}
+
 /* NAV */
 const searchMenu = document.getElementById("search");
 const mainMenu = document.getElementById("main-menu");
 
+const inputCellphone = document.getElementById("input-cellphone");
+const inputTablet = document.getElementById("input-tablet");
+const inputDesktop = document.getElementById("input-desktop");
+
     /* Buttons */
-const searchIcon = document.getElementById("search-icon");
+const searchIconCellphone = document.getElementById("search-icon-cellphone");
+const searchIconTablet = document.getElementById("search-icon-tablet");
+const searchIconDesktop = document.getElementById("search-icon-desktop");
 
 const menuIconDisplay = document.getElementById("menu-icon");
 const closeMenu = document.getElementById("close-menu-icon");
 
-    /* Functions */
-    
+    /* Event Listener */
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Enter"){
+        if (inputCellphone.value != "" && inputCellphone.value != " ") { 
+            window.location.href = `products.html#${inputCellphone.value}`;      
+        } else if (inputTablet.value != "" && inputTablet.value != " ") {
+            window.location.href = `products.html#${inputTablet.value}`;
+        } else if (inputDesktop.value != "" && inputDesktop.value != " ") {
+            window.location.href = `products.html#${inputDesktop.value}`;
+        }
+    };
+},false);
+
+searchIconCellphone.addEventListener("click", () => {
+    openNewTab();
+});
+searchIconTablet.addEventListener("click", () => {
+    openNewTab();
+});
+searchIconDesktop.addEventListener("click", () => {
+    openNewTab();
+});
+
         /* Search Menu - Display or Hide */
-searchIcon.addEventListener("click", () => {
+searchIconCellphone.addEventListener("click", () => {
     replaceClass (searchMenu, "displayFlex", "displayHidden");
 });
 
@@ -34,6 +71,7 @@ closeMenu.addEventListener("click", () => {
     mainMenu.classList.add("displayNone");
 });
 
+/* MAIN */
 /* Arrows Buttons */
 const arrowLeft = document.getElementById("arrow_left");
 const arrowRight = document.getElementById("arrow_right")
